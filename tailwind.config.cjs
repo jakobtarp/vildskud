a066d319ab6f43cc87cfda90b30d9f2c2c158ec9
Jakob Tarp <jakob.tarp@outlook.com>
2023-03-26 16:19:52 +0200

    nobg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        hero: "url('../public/img/iPhone_.png')",
-        rebirth: "url('../public/img/rebirth.png')",
-        mandligger: "url('../public/img/mandligger.png')",
-        rebirth: "url('../public/img/rebirth.png')",
-        rebirth: "url('../public/img/rebirth.png')",
-        rebirth: "url('../public/img/rebirth.png')",
-        rebirth: "url('../public/img/rebirth.png')",
-      },
+      // backgroundImage: {
+      //   hero: "url('../public/img/iPhone_.png')",
+      //   rebirth: "url('../public/img/rebirth.png')",
+      //   mandligger: "url('../public/img/mandligger.png')",
+      //   rebirth: "url('../public/img/rebirth.png')",
+      //   rebirth: "url('../public/img/rebirth.png')",
+      //   rebirth: "url('../public/img/rebirth.png')",
+      //   rebirth: "url('../public/img/rebirth.png')",
+      // },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
